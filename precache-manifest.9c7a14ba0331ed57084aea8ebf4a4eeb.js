e7842db4ae90cda32cf466dcc77010da8a152f06
Alex Boicu <boicualexandru@mail.com>
2020-03-03 17:18:55 +0200

    deploy

---
+++ precache-manifest.9c7a14ba0331ed57084aea8ebf4a4eeb.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ede4a02e552d5770aa1a487e04f0056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b27713ce62ba85023cbb",
+    "url": "/static/css/2.8f0d8aca.chunk.css"
+  },
+  {
+    "revision": "6e24980394517d194e95",
+    "url": "/static/css/main.21967c7c.chunk.css"
+  },
+  {
+    "revision": "b27713ce62ba85023cbb",
+    "url": "/static/js/2.923421b9.chunk.js"
+  },
+  {
+    "revision": "6e24980394517d194e95",
+    "url": "/static/js/main.1df3c68b.chunk.js"
+  },
+  {
+    "revision": "637a7aeb85c48549849f",
+    "url": "/static/js/runtime-main.dbcba924.js"
+  },
+  {
+    "revision": "38399681c789c5f4b02bfd2f47038f93",
+    "url": "/static/media/MacBookPro_iphone_restaurant_SMALL.38399681.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "589f6ee31af7dda03d2bd7929784dfb9",
+    "url": "/static/media/browser_mockup_restaurants.589f6ee3.png"
+  },
+  {
+    "revision": "edee495091704bbaa4078bd5be78bae8",
+    "url": "/static/media/browser_phone_hotel.edee4950.png"
+  },
+  {
+    "revision": "b578893f27571082b09ee34a256c53c3",
+    "url": "/static/media/browser_phone_login.b578893f.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "5948a28df5163f333e9d75b17bf0a295",
+    "url": "/static/media/heroImage.5948a28d.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "4ed90c9d0400bfd5830c6c65f3007da6",
+    "url": "/static/media/moons_icon.4ed90c9d.png"
+  },
+  {
+    "revision": "d48b4990d509edda0d20c18b351fd758",
+    "url": "/static/media/moons_included_services.d48b4990.png"
+  },
+  {
+    "revision": "ea44cca684975eb893750d72594aecb4",
+    "url": "/static/media/moons_locations.ea44cca6.png"
+  },
+  {
+    "revision": "e396719f25cd98700d965b91e661ae79",
+    "url": "/static/media/moons_menu_modal.e396719f.png"
+  },
+  {
+    "revision": "2cfc88451863d81471f39e727271af29",
+    "url": "/static/media/moons_menus.2cfc8845.png"
+  },
+  {
+    "revision": "3c857f5d1156dd327c5261ffd5902600",
+    "url": "/static/media/moons_room_modal.3c857f5d.png"
+  },
+  {
+    "revision": "e9279bf765cb9ae66f8259eaa2b52837",
+    "url": "/static/media/moons_rooms.e9279bf7.png"
+  },
+  {
+    "revision": "a78d00efe8d5bc5f9215508ef5fa89f9",
+    "url": "/static/media/profileBlob.a78d00ef.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
